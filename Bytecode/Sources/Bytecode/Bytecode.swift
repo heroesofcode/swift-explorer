ac038eb5b05c8214a96513fe6be658a88a8e57be
joaolfp <joaolucasfp2001@gmail.com>
2024-06-22 08:01:21 -0300

    Fix scheme and move code to bytecode

--- Bytecode/Sources/Bytecode/Bytecode.swift
+++ Bytecode/Sources/Bytecode/Bytecode.swift
@@ -1,2 +1,73 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+
+public final class Bytecode {
+    
+    @Binding private var swiftCode: String
+    @Binding private var bytecode: String
+    
+    public init(swiftCode: Binding<String>, bytecode: Binding<String>) {
+        self._swiftCode = swiftCode
+        self._bytecode = bytecode
+    }
+    
+    public func generateBytecode() {
+        let (tempFile, outputFile) = validationFieldSwiftCode()
+        
+        let pipe = processArgument(tempFile: tempFile, outputFile: outputFile)
+        
+        let errorData = pipe.fileHandleForReading.readDataToEndOfFile()
+        let errorOutput = String(data: errorData, encoding: .utf8) ?? ""
+        
+        print("Error Output: \(errorOutput)")
+        
+        result(errorOutput: errorOutput, outputFile: outputFile)
+    }
+    
+    private func validationFieldSwiftCode() -> (String, String) {
+        let tempFile = NSTemporaryDirectory() + "tempfile.swift"
+        let outputFile = NSTemporaryDirectory() + "output.s"
+        
+        let correctedSwiftCode = swiftCode.replacingOccurrences(of: "“", with: "\"")
+            .replacingOccurrences(of: "”", with: "\"")
+        
+        do {
+            try correctedSwiftCode.write(toFile: tempFile, atomically: true, encoding: .utf8)
+        } catch {
+            bytecode = "Error writing temporary file: \(error.localizedDescription)"
+            return (tempFile, outputFile)
+        }
+        
+        return (tempFile, outputFile)
+    }
+    
+    private func processArgument(tempFile: String, outputFile: String) -> Pipe {
+        let process = Process()
+        process.launchPath = "/usr/bin/env"
+        process.arguments = ["swiftc", "-emit-assembly", tempFile, "-o", outputFile]
+        
+        let pipe = Pipe()
+        let errorPipe = Pipe()
+        process.standardOutput = pipe
+        process.standardError = errorPipe
+        
+        process.launch()
+        process.waitUntilExit()
+        
+        return errorPipe
+    }
+    
+    private func result(errorOutput: String, outputFile: String) {
+        if !errorOutput.isEmpty {
+            bytecode = "Error during script execution: \(errorOutput)"
+        } else {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: outputFile))
+                bytecode = String(data: data, encoding: .utf8) ?? "Error reading bytecode"
+            } catch {
+                bytecode = "Error reading output file: \(error.localizedDescription)"
+            }
+        }
+    }
+
+}
--- SwiftExplorer/Features/Home/ContentView.swift
+++ SwiftExplorer/Features/Home/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CodeEditor
+import Bytecode
 
 struct ContentView: View {
     @State private var swiftCode: String = ""
@@ -49,7 +50,8 @@ struct ContentView: View {
             .padding(.top, 16)
             
             Button(action: {
-                generateBytecode()
+                let bytecode = Bytecode(swiftCode: $swiftCode, bytecode: $bytecode)
+                bytecode.generateBytecode()
             }) {
                 Text("Generate Bytecode")
                     .foregroundColor(.white)
@@ -81,51 +83,6 @@ struct ContentView: View {
         }
         .padding()
     }
-
-    func generateBytecode() {
-        let tempFile = NSTemporaryDirectory() + "tempfile.swift"
-        let outputFile = NSTemporaryDirectory() + "output.s"
-        
-        let correctedSwiftCode = swiftCode.replacingOccurrences(of: "“", with: "\"")
-            .replacingOccurrences(of: "”", with: "\"")
-        
-        do {
-            try correctedSwiftCode.write(toFile: tempFile, atomically: true, encoding: .utf8)
-        } catch {
-            bytecode = "Error writing temporary file: \(error.localizedDescription)"
-            return
-        }
-        
-        let process = Process()
-        process.launchPath = "/usr/bin/env"
-        process.arguments = ["swiftc", "-emit-assembly", tempFile, "-o", outputFile]
-        
-        let pipe = Pipe()
-        let errorPipe = Pipe()
-        process.standardOutput = pipe
-        process.standardError = errorPipe
-        
-        process.launch()
-        process.waitUntilExit()
-        
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        
-        let errorOutput = String(data: errorData, encoding: .utf8) ?? ""
-        
-        print("Error Output: \(errorOutput)")
-        
-        if !errorOutput.isEmpty {
-            bytecode = "Error during script execution: \(errorOutput)"
-        } else {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: outputFile))
-                bytecode = String(data: data, encoding: .utf8) ?? "Error reading bytecode"
-            } catch {
-                bytecode = "Error reading output file: \(error.localizedDescription)"
-            }
-        }
-    }
-
 }
 
 extension Color {
