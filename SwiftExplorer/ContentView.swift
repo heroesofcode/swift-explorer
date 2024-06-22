95f0e1787ca1f41deb0c37df36256cd0b9cbafa4
joaolfp <joaolucasfp2001@gmail.com>
2024-06-22 03:15:50 -0300

    Initial commit

---
+++ SwiftExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftExplorer
+//
+//  Created by João Lucas on 22/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftExplorer/SwiftExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExplorerApp.swift
+//  SwiftExplorer
+//
+//  Created by João Lucas on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
