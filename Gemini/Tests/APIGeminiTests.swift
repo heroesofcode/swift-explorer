91e10626b90b20601c3a51866f28841cc9b1c88d
joaolfp <joaolucasfp2001@gmail.com>
2024-09-18 01:34:38 -0300

    Fix unit test API Gemini Mock

--- Gemini/Tests/APIGeminiTests.swift
+++ Gemini/Tests/APIGeminiTests.swift
@@ -2,63 +2,42 @@ import XCTest
 @testable import Gemini
 
 final class APIGeminiTests: XCTestCase {
-    
-    var mockAPIGemini: APIGeminiMock!
-    
+
+    var apiGemini: APIGemini!
+
     override func setUp() {
         super.setUp()
-        mockAPIGemini = APIGeminiMock()
+        apiGemini = APIGemini()
     }
-    
+
     override func tearDown() {
-        mockAPIGemini = nil
+        apiGemini = nil
         super.tearDown()
     }
     
-    func testResultSuccess() async {
+    func testResultFailure() async throws {
         let expectation = XCTestExpectation(description: "Completion handler invoked")
-        mockAPIGemini.mockResponseText = "Mocked successful response"
-        
 
-        await mockAPIGemini.result(
-            apiKey: "dummyApiKey",
-            code: "swift code",
-            llvmCode: "llvm code",
-            assemblyCode: "assembly code"
-        ) { result in
-            switch result {
-            case .success(let text):
-                // Assert
-                XCTAssertEqual(text, "Mocked successful response")
-                expectation.fulfill()
-            case .failure:
-                XCTFail("Expected success but got failure")
-            }
-        }
-        
-        await fulfillment(of: [expectation], timeout: 1.0)
-    }
-    
-    func testResultFailure() async {
-        let expectation = XCTestExpectation(description: "Completion handler invoked")
-        mockAPIGemini.shouldReturnError = true
-        
-        await mockAPIGemini.result(
-            apiKey: "dummyApiKey",
-            code: "swift code",
-            llvmCode: "llvm code",
-            assemblyCode: "assembly code"
+        let mockInvalidApiKey = "invalidApiKey"
+        let mockSwiftCode = "print('Hello World')"
+        let mockLLVMCode = "llvm code"
+        let mockAssemblyCode = "assembly code"
+
+        await apiGemini.result(
+            apiKey: mockInvalidApiKey,
+            code: mockSwiftCode,
+            llvmCode: mockLLVMCode,
+            assemblyCode: mockAssemblyCode
         ) { result in
             switch result {
             case .success:
-                XCTFail("Expected failure but got success")
+                XCTFail("Success")
             case .failure(let error):
-                // Assert
-                XCTAssertEqual(error.localizedDescription, "Mocked error")
+                XCTAssertNotNil(error, "Error")
                 expectation.fulfill()
             }
         }
-        
+
         await fulfillment(of: [expectation], timeout: 1.0)
     }
 }
